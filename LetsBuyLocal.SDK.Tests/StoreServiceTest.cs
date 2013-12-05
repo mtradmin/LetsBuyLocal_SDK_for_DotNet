1603192481d6254b7ab6230eaa8e2aa72fc01065
letsbuylocal <rewards123>
2013-12-05 10:04:41 -0600

    Resolved conflict by keeping App.config and added files that were not tracked

---
+++ LetsBuyLocal.SDK.Tests/StoreServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using LetsBuyLocal.SDK.Models;
+using LetsBuyLocal.SDK.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LetsBuyLocal.SDK.Tests
+{
+    [TestClass]
+    public class StoreServiceTest
+    {
+        //[TestMethod]
+        //public void CreateStoreTest()
+        //{
+        //    Store store = new Store();
+        //    store.Website = "http://www.zebrastore.com";
+        //    store.Name = "Zebra's of All Stripes";
+        //    store.Phone = "2623491479";
+        //    store.AddressLine1 = "10 Zebra Dr.";
+        //    store.City = "Zebraton";
+        //    store.State = "WI";
+        //    store.Zip = "53186";
+        //    store.TimeZone = "CST";
+
+        //    StoreService svc = new StoreService();
+        //    var resp = svc.CreateStore(store);
+        //    Assert.IsTrue(resp.Success);
+        //}
+    }
+}
---
+++ LetsBuyLocal.SDK.Tests/UserServiceTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Configuration;
+using System.Text;
+using LetsBuyLocal.SDK.Models;
+using LetsBuyLocal.SDK.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LetsBuyLocal.SDK.Tests
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        [TestMethod]
+        public void CreateUserTest()
+        {
+            User user = new User();
+            string baseEmailName = ConfigurationManager.AppSettings["BaseEmailName"];
+            string atEmail = ConfigurationManager.AppSettings["AtEmail"];
+
+            user.Email = GetEmailAlias(baseEmailName, "@gmail.com");
+            user.Password = GetRandomString(7);
+            user.FirstName = GetRandomString(5);
+            user.LastName = GetRandomString(7); ;
+            user.MobilePhoneNumber = GetRandomString(10);
+        
+            UserService svc = new UserService();
+            var resp = svc.CreateUser(user);
+
+            Assert.IsNotNull(resp.Object);
+        }
+
+        [TestMethod]
+        public void GetUserByIdTest()
+        {
+            string id = "3872417b-3657-47df-8661-e13557a056d3";
+            UserService svc = new UserService();
+            var resp = svc.GetUserById(id);
+            Assert.IsNotNull(resp.Object);
+        }
+
+
+
+
+        private string GetRandomString(int len)
+        {
+            Random r = new Random();
+            string str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+            StringBuilder sb = new StringBuilder();
+            while ((len) > 0)
+            {
+                sb.Append(str[(int) r.NextDouble()*str.Length]);
+                len--;
+            }
+            return sb.ToString();
+        }
+
+        private string GetEmailAlias(string baseEmailName, string atEmail)
+        {
+            StringBuilder sb = new StringBuilder();
+            string s = string.Empty;
+            Guid guid = Guid.NewGuid();
+
+            sb.Append(baseEmailName);
+            sb.Append("+");
+            sb.Append(guid.ToString());
+            sb.Append(atEmail);
+            s = sb.ToString();
+            return s;
+        }
+
+        
+    }
+}
