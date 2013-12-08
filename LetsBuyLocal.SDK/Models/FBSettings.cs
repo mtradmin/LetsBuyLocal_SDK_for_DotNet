522676ae48d71dd3512fef77ac61ce176b3ad2be
letsbuylocal <rewards123>
2013-12-08 10:24:41 -0600

    Add new model classes

---
+++ LetsBuyLocal.SDK/Models/FBSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsBuyLocal.SDK.Models
+{
+    public class FBSettings
+    {
+        public string Account { get; set; }
+        public string Page { get; set; }
+        public string PageAccessToken { get; set; }
+        public bool PublishAlerts { get; set; }
+        public bool PublishDeals { get; set; }
+    }
+}
---
+++ LetsBuyLocal.SDK/Models/GeoPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsBuyLocal.SDK.Models
+{
+    public class GeoPoint
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
