d1e92a2894e2f119ac69cd39ebd10a1197d80918
letsbuylocal <rewards123>
2013-12-09 10:57:20 -0600

    Add DateParameter model

---
+++ LetsBuyLocal.SDK/Models/DateParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsBuyLocal.SDK.Models
+{
+    public class DateParameter
+    {
+        public DateTime datetime { get; set; }
+    }
+}
